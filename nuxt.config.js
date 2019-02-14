f3d65a82e4800348646a5aa58b8cdf5ae6c06972
igor <perrofeofilms@gmail.com>
2019-02-13 19:33:27 -0600

    -

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Food Forest with Ron Hunter and James Brown',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
